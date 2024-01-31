7c05834da8c9eb8aae81403f013ede42fc1e0ec4
Igor Guerra <igor.vguerra@hotmail.com>
2024-01-31 15:12:04 +0000

    working on the JS

--- JS/favorites.js
+++ JS/favorites.js
@@ -1,6 +1,51 @@
+export class GithubUser {
+    static search(username) {
+        const endpoint = `https://api.github.com/users/${username}`
+            return fetch(endpoint)
+            .then(data => data.json())
+            .then(({ login, name, public_repos, followers }) => ({
+                login,
+                name,
+                public_repos,
+                followers
+
+        }))
+    }
+}
+
+
+
+
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
+        this.load()
+
+        GithubUser.search('igorvguerra')
+        .then(user => console.log(user))
+    }
+
+    load() {
+        this.entries = 
+        [
+            {
+            login: 'maykbrito',
+            name: "Mayk Brito",
+            public_repos: '76',
+            followers: '12000'
+            },
+            {
+            login: 'diego3g',
+            name: "Diego Fernandes",
+            public_repos: '48',
+            followers: '22503',
+            }
+        ]    
+    }
+
+    delete(user) {
+        const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
     }
 }
 
@@ -9,18 +54,58 @@ export class FavoritesView extends Favorites {
     constructor(root) {
         super(root)
 
+        this.tbody = this.root.querySelector('table tbody')
+
         this.update()
     }
 
     update() {
         this.removeAllTr()
+
+        
+        this.entries.forEach( user => {
+            const row = this.createRow()
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+                if(isOk) {
+                    this.delete(user)
+                }
+            }
+
+            this.tbody.append(row)
+        })
     }
 
     removeAllTr() {
-        const tbody = this.root.querySelector('table tbody')
-
-        tbody.querySelectorAll('tr').forEach((tr) => {
+        
+        this.tbody.querySelectorAll('tr').forEach((tr) => {
             tr.remove()
         })
+    }
+
+    createRow() {
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+        <td class="user">
+            <img src="https://github.com/maykbrito.png" alt="Imagem de Mayk Brito">
+            <a href="https://github.com/maykbrito" target="_blank">
+                <p>Mayk Brito</p>
+                <span>maykbrito</span>
+            </a>
+        </td>
+        <td class="repositories">76</td>
+        <td class="followers">9589</td>
+        <td><button class="remove">&times;</button></td>
+    `
+        return tr   
     }
 }
